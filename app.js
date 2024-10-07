1e0b24d48a00b646560ccea74862d133a3acef41
Ahmed <ahmedanari2001@gmail.com>
2024-10-07 07:33:39 +0500

    production ready

--- app.js
+++ app.js
@@ -33,8 +33,7 @@ app.use(
     origin:
       process.env.NODE_ENV === "development"
         ? "http://localhost:5173"
-        : // : "https://tt-pro.onrender.com",
-          "http://localhost:8000",
+        : "https://tt-pro.onrender.com",
 
     credentials: true,
   })
--- client/lib/data.js
+++ client/lib/data.js
@@ -10,9 +10,10 @@ export const links = [
 ];
 
 let url;
-if (import.meta.env.VITE_ENV === "production") {
+if (import.meta.env.VITE_ENV === "development") {
   url = "http://localhost:8000/api/v1";
 } else {
+  // url = "https://tt-pro.onrender.com/api/v1";
   url = "https://tourtales-p02r.onrender.com/api/v1";
 }
 export { url };
